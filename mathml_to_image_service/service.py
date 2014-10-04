b9ad2e35cc9ab3f5e8fe8513f15c7838dc1ad017
William Stewart <william@synthasite.com>
2014-10-04 18:59:06 +0200

    Use native KeyError

--- mathml_to_image_service/service.py
+++ mathml_to_image_service/service.py
@@ -7,26 +7,18 @@ import svg_to_image
 app = Flask(__name__)
 
 
-def safe_get_field(field):
-    if field in request.form:
-        return request.form[field]
-    raise KeyError(field)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def convert():
     if request.method == 'GET':
         return jsonify(error='Try sending MathML in a POST request. :)')
     try:
-        mathml = safe_get_field('mathml')
-        format = safe_get_field('image_format')
-        max_size = safe_get_field('max_size')
+        mathml = request.form['mathml']
+        format = request.form['image_format']
+        max_size = request.form['max_size']
     except KeyError as error:
-        return jsonify(error='Missing field: %s' % error, status=400)
+        return jsonify(error='Missing field: %s' % error.args[0], status=400)
 
     svg_string = get_svg(mathml)
-
-    print('svg strung')
 
     file_name = svg_to_image.to_image(svg_string, format, int(max_size))
 
