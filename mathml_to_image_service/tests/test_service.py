54ca77211e690127e54208a8037445e12ac9fb63
William Stewart <william@synthasite.com>
2014-10-05 09:04:44 +0200

    Move tests into their own dir

--- mathml_to_image_service/tests/test_service.py
+++ mathml_to_image_service/tests/test_service.py
@@ -1,14 +1,14 @@
 import json
 import unittest
 
-import service
+from mathml_to_image_service import service
 
 
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         self.app = service.app.test_client()
 
-    def test_root_path(self):
+    def test_get_root_path(self):
         self.assertEqual(
             json.loads(self.app.get('/').data.decode('utf-8')),
             {'error': 'Try sending MathML in a POST request. :)'})
