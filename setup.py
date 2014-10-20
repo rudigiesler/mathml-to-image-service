6b21f6e6e5b0bcbbb0b8d814c81f6440b6ee99b1
Rudi Giesler <rudi@praekelt.com>
2014-10-20 13:28:20 +0200

    Added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,21 @@ setup(
     author='',
     author_email='',
     url='https://github.com/rudigiesler/mathml-to-image-service/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+    ],
     packages=find_packages(),
     install_requires=required
 )
